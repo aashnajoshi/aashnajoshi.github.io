30779f1343dff7d496ceb1e2e89c6aeb955d7980
aashnajoshi <aashnahetal@gmail.com>
2024-03-30 17:10:31 +0530

    .

--- toggle.js
+++ toggle.js
@@ -1,23 +1,31 @@
+const toggler = document.body;
+const darkModeButton = document.getElementById('dark-mode');
+
 function toggleLightMode() {
-    const app = document.body;
     if (localStorage.lightMode === "dark") {
         localStorage.lightMode = "light";
-        app.setAttribute("data-light-mode", "light");
+        toggler.setAttribute("data-light-mode", "light");
     } else {
         localStorage.lightMode = "dark";
-        app.setAttribute("data-light-mode", "dark");
+        toggler.setAttribute("data-light-mode", "dark");
     }
     console.log("lightMode = " + localStorage.lightMode);
 }
 
-document.getElementById('dark-mode').addEventListener('click', function (e) {
-    const toggler = document.body;
+darkModeButton.addEventListener('click', function (e) {
     toggler.classList.toggle('dark-mode');
-    const target = e.target;
-    target.classList.toggle('fa-moon');
-    target.classList.toggle('fa-sun');
+    darkModeButton.querySelector('i').classList.toggle('fa-moon');
+    darkModeButton.querySelector('i').classList.toggle('fa-sun');
 });
 
 if (localStorage.lightMode === "dark") {
-    document.body.setAttribute("data-light-mode", "dark");
+    toggler.setAttribute("data-light-mode", "dark");
 }
+
+const elementsToDisableRightClick = document.querySelectorAll('.prevent-right-click');
+
+elementsToDisableRightClick.forEach(element => {
+    element.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+});
